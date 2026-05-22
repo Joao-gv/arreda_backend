aed9ec4834df8a4c59cb482e5f8bb5fde22178dd
Joao <joao.vieira@tcsindustrial.com.br>
2026-05-21 21:10:39 -0300

    feat:adiciona-UsuarioService

---
+++ src/main/java/br/com/arreda/backend/services/UsuarioService.java
@@ -0,0 +1,38 @@
+package br.com.arreda.backend.services;
+import br.com.arreda.backend.models.Usuario;
+import br.com.arreda.backend.repositories.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final  UsuarioRepository repository;
+
+    public boolean salvarUsuario(@NonNull Usuario usuario){
+
+        if (usuario.getNome() == null || usuario.getNome().trim().isEmpty()) {
+            throw new IllegalArgumentException("Erro: O nome do usuário é obrigatório.");
+        }
+
+        if (usuario.getSenha() == null || usuario.getSenha().trim().isEmpty()) {
+            throw new IllegalArgumentException("Erro: A senha é obrigatória.");
+        }
+
+        if (usuario.getSenha().length() < 6) {
+            throw new IllegalArgumentException("Erro: A senha deve conter no mínimo 6 caracteres.");
+        }
+
+        boolean existingUserEmail = repository.existsByEmail(usuario.getEmail());
+
+        if (existingUserEmail) {
+            throw new IllegalArgumentException("Erro: O e-mail informado já está cadastrado.");
+        }
+
+        repository.save(usuario);
+        return true;
+
+    }
+}
