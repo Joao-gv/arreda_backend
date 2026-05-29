826d6519f2a08d8835a13008ba528a18c7e95b29
Joao-gv <pedrinhogv2122@gmail.com>
2026-05-29 19:11:14 -0300

    refactor: SecurityConfig libera todas as rotas

--- src/main/java/br/com/arreda/backend/config/SecurityConfig.java
+++ src/main/java/br/com/arreda/backend/config/SecurityConfig.java
@@ -18,15 +18,16 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // Este Bean diz ao Spring quais rotas são públicas e quais são bloqueadas
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // Desativa proteção CSRF (necessário para APIs REST)
+                .csrf(csrf -> csrf.disable()) // Desabilita proteção contra CSRF (comum em APIs REST)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/usuarios").permitAll() // Libera geral a sua rota de cadastro
-                        .anyRequest().authenticated() // Bloqueia todo o resto
-                );
+                        .anyRequest().permitAll() // Permite QUALQUER requisição (Swagger, Controllers, etc.)
+                )
+                .formLogin(form -> form.disable()) // Desativa essa tela de login da imagem
+                .httpBasic(basic -> basic.disable()); // Desativa o pop-up de login básico
+
         return http.build();
     }
 }
