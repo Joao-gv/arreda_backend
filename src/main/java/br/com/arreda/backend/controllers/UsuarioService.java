b97b30a0f9fd7e2977ae9063701da2f973399eb2
Carlos Eduardo <carloseduardozimbra@gmail.com>
2026-05-28 21:30:33 -0300

    salvando os arquivos antes de trazer o DTO

---
+++ src/main/java/br/com/arreda/backend/controllers/UsuarioService.java
@@ -0,0 +1,6 @@
+package br.com.arreda.backend.controllers;
+
+import br.com.arreda.backend.services.UsuarioCreateDTO;
+public class UsuarioService {
+
+}
--- src/main/java/br/com/arreda/backend/services/UsuarioService.java
+++ src/main/java/br/com/arreda/backend/services/UsuarioService.java
@@ -4,12 +4,14 @@ import br.com.arreda.backend.repositories.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
 
     private final  UsuarioRepository repository;
+    private final PasswordEncoder passwordEnconder;
 
     public boolean salvarUsuario(@NonNull Usuario usuario){
 
@@ -25,11 +27,18 @@ public class UsuarioService {
             throw new IllegalArgumentException("Erro: A senha deve conter no mínimo 6 caracteres.");
         }
 
+        if (usuario.getEmail() != null){
+            usuario.setEmail(usuario.getEmail().toLowerCase());
+        }
+
         boolean existingUserEmail = repository.existsByEmail(usuario.getEmail());
 
         if (existingUserEmail) {
             throw new IllegalArgumentException("Erro: O e-mail informado já está cadastrado.");
         }
+
+        String senhaCriptograda = passwordEnconder.encode(usuario.getSenhaHash());
+        usuario.setSenhaHash(senhaCriptograda);
 
         repository.save(usuario);
         return true;
